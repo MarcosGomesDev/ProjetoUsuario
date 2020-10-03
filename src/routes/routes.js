88091f176133aa7b1861c9ba433fdab0de5ab6a4
MarcosPauloSantosGomes <marcospaulosantosgomes@gmail.com>
2020-10-03 19:15:12 -0300

    Backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,3 +5,9 @@ const router = express.Router();
 router.post('/usuarios', UsuarioController.Insert);
  
 module.exports = router;
+
+router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SelectAll);
+router.get('/usuarios/:id', UsuarioController.SelectDetail);
+
+module.exports = router;
