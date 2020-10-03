7e58d8baa862c18d4b852de1b803c3da18241c72
MarcosPauloSantosGomes <marcospaulosantosgomes@gmail.com>
2020-10-03 19:20:09 -0300

    Backend com Update e Delete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,5 +9,7 @@ module.exports = router;
 router.post('/usuarios', UsuarioController.Insert);
 router.get('/usuarios', UsuarioController.SelectAll);
 router.get('/usuarios/:id', UsuarioController.SelectDetail);
+router.put('/usuarios/:id', UsuarioController.Update);
+router.delete('/usuarios/:id', UsuarioController.Delete);
 
 module.exports = router;
